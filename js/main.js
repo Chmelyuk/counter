aaa83e5f0b00d558a56a8257198112dfe9f3a3d2
Chmelyuk <98876964+Chmelyuk@users.noreply.github.com>
2024-09-30 21:28:05 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const { useEffect, useState } = React;
+const rootElement = document.getElementById('app');
+
+
+function App() {
+    const [count, setCount]  = useState( );
+    const [Dislike, counter]  = useState(0);
+    
+    function Increment(){
+        setCount(count + 1)
+
+    }
+
+    function Decrement(){
+        counter(Dislike + 1)
+    }
+
+    return( 
+        <>
+        <header></header>
+            <div className = 'appka'>
+                <h1>Counter likes</h1>
+                <h1><span>❤</span> {count}</h1>
+                <h1>💔{Dislike}</h1>
+    <button onClick={Increment}>Like 👍</button> 
+    <button onClick={Decrement}>Dislike 👎🏿</button>
+     </div>
+      
+      </>);
+};
+
+
+  
+ReactDOM.createRoot(rootElement).render(<App />);
